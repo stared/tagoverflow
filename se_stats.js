ae99ba93d0eda3a55861e83cb5c9f8a74807b393
Marta Czarnocka-Cieciura <martaczc@interia.pl>
2014-02-16 19:02:30 +0100

    stats in separate file, colors

---
+++ se_stats.js
@@ -0,0 +1,31 @@
+/*
+SETagStats = function (questions) {
+  this.questions = questions;
+  
+  this.mean = function (field) {
+  
+  }
+  
+  this.answered = {};
+}
+*/
+
+function answered(questions) 
+{
+  var questionNumber=questions.length;
+  var answeredNumber=questions.filter(function(x) {return x.is_answered;}).length;
+  if (questionNumber>0) {return answeredNumber/questionNumber}
+  else {return 0};
+}
+
+function questionsScore(questions) {
+  return d3.mean(questions, function (q) { return q.score; });
+}
+
+function questionsView(questions){
+  return d3.mean(questions, function (q) { return q.view_count; });
+}
+
+function ownerReputation(questions){
+  return d3.mean(questions, function (q) { return q.owner.reputation; });
+}
