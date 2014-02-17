c212cc69065f9e8abfe013cad006f9031db8b8fd
Piotr Migdal <pmigdal@gmail.com>
2014-02-17 21:24:29 +0100

    ids again

--- se_tags.js
+++ se_tags.js
@@ -216,7 +216,7 @@ var SeDataLoaderPerSite = function(siteName, tagLimit, delay){
     this.siteStats = fetchSiteStats(siteName)[0];
     $(".site_info #site_name").html(this.siteData.name);
     $(".site_info #dscr").html(this.siteData.audience);
-    $(".site_info a").attr("href", this.siteData.site_url);
+    $(".site_info #site_name").attr("href", this.siteData.site_url);
     this.retriveTags();
     this.retriveRelatedTags();
   };
